8e1b61f57e54751353b20334b457ddd8d4c253cf
github username <github email address>
2018-10-20 13:15:02 -0500

    Github initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
+import Login from './Components/login-form.js'
+import LoginPage from './Components/login-page.js'
+import TimesheetCreation from './Components/timesheet-creation.js'
+import CompanyNavbar from './Components/navbar.js'
+import { connect } from 'react-redux';
+import { Button, FormGroup, FormControl, ControlLabel, Col, Grid, Row} from "react-bootstrap";
 import './App.css';
 
 class App extends Component {
+
   render() {
+
+    var homepageComponent = <LoginPage />
+
+    if (localStorage.user) {
+      homepageComponent = <div><CompanyNavbar /><TimesheetCreation /></div>
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {homepageComponent}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state)
+  if (state.session.authenticated === true) {
+    return  {
+      authenticated: true,
+      name: state.session.user.username,
+      user: state.session.user
+    }
+  } else {
+    return  {
+      authenticated: false,
+      name: "anonymous",
+      user: {}
+    }
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Amplify from "aws-amplify";
+import { sessionReducer } from 'redux-react-session';
+import { sessionService } from 'redux-react-session';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { combineReducers } from 'redux'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = {
+  session: sessionReducer
+};
+const reducer = combineReducers(reducers);
+
+const store = createStore(reducer)
+
+sessionService.initSessionService(store, { driver: 'COOKIES' });
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: 'us-west-2',
+    userPoolId: 'us-west-2_uRvzvmQ8M',
+    userPoolWebClientId: '4qc73d3q2dv0cfr7cg3q4lm7j1'
+  }
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App store={store}/>
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
